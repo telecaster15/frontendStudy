b3f88902015d5fc91bbf39111bac573101ae7a59
205 <illionaire89@gmail.com>
2020-03-31 16:53:17 +0900

    0331

---
+++ HTML/js/googlemap.js
@@ -0,0 +1,8 @@
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.485795, lng: 126.897242},
+    zoom: 17
+  });
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## HTML&CSS&JSP
+
+## https://poiemaweb.com/
+
+## 0330 : HTML% 기본 문법, 시멘틱 요소와 검색 엔진. 웹페이지의 구성하는 기본 태그, 텍스트 관련 태그, HTML의 핵심 개념인 Hyperlink
+
